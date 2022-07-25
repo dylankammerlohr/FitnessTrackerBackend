861fe96d9c325244372a2650331b4d6c79b0ece1
PrestonS95 <prestonsullins95@gmail.com>
2022-07-25 10:46:33 -0500

    Added the createUser function and got the createusertest to complete

--- db/users.js
+++ db/users.js
@@ -1,13 +1,35 @@
 const client = require("./client");
+const bcrypt = require("bcrypt")
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
+  // const SALT_COUNT = 10;
+  // const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+  try {
+    const {rows: [user]} = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING *;
+    `, [username, password])
+
+    return user
+  } catch (error) {
+    console.error("Problems creating user..")
+    throw error
+  }  
+
   
 }
 
 async function getUser({ username, password }) {
+const user = await getUserByUserName(username);
+// const hashedPassword = user.password;
+// const isValid = await bcrypt.compare(password, hashedPassword);
+
+
 
 }
 
