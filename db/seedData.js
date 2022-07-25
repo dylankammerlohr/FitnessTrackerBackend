0d8d6524f67f7fa686b100b98f9bbc4b8cc30657
Dylan Kammerlohr <dylankammerlohr@gmail.com>
2022-07-25 10:09:27 -0500

    built out dropTables and createTables for db

--- db/seedData.js
+++ db/seedData.js
@@ -1,15 +1,58 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
-// const { } = require('./');
+const {createActivity, createRoutine, createUser, getAllActivities, getRoutinesWithoutActivities, addActivityToRoutine } = require('./');
 const client = require("./client")
 
 async function dropTables() {
-  console.log("Dropping All Tables...")
-  // drop all tables, in the correct order
+  console.log("Starting to drop tables...")
+  try {
+    await client.query(`
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS activities;
+    DROP TABLE IF EXISTS routines;
+    DROP TABLE IF EXISTS routine_activities;
+    `);
+    console.log('Finished dropping tables')
+  } catch (error){
+    console.error('Error dropping tables')
+    throw error 
+  }
 }
 
 async function createTables() {
   console.log("Starting to build tables...")
-  // create all tables, in the correct order
+  try {
+    await client.query(`
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL
+    );
+    CREATE TABLE activities(
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
+    );
+    CREATE TABLE routines(
+      id SERIAL PRIMARY KEY,
+      "createrId" INTEGER REFERENCES users(id),
+      "isPublic" BOOLEAN DEFAULT false,
+      name varchar(255) UNIQUE NOT NULL,
+      goal TEXT NOT NULL
+    );
+    CREATE TABLE routine_activites(
+       id SERIAL PRIMARY KEY,
+       "routineId" INTEGER REFERENCES routines(id),
+       "activityId" INTEGER REFERENCES activities(id),
+       duration INTEGER,
+       count INTEGER,
+       UNIQUE ("routineId", "activityId")
+    );
+    `);
+    console.log('Finished creating tables')
+  } catch (error){
+    console.error('Error creating tables')
+    throw error 
+  }
 }
 
 /* 
